f331d046d90571508410b6c1fbe088cdb3846499
YosephKurubinganBekayo <bekayokurnia@gmail.com>
2024-12-11 07:39:56 +0700

    PSIM

--- navbar.php
+++ navbar.php
@@ -1,60 +1,45 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-transparent position-fixed w-100">
+<?php
+$current_page = basename($_SERVER['PHP_SELF']); // Mendapatkan nama file saat ini
+?>
+
+<nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
   <div class="container">
-    <?php
-    $profile = $mysqli->get_show_profile(); // Panggil fungsi untuk mendapatkan data
-    if ($profile) { ?>
-      <a class="navbar-brand" href="#">
-        <img src="img/buku.jpg" alt="" width="30" height="24" class="d-inline-block align-text-top">
-        <?php echo $profile['titlewebsite'] ?></a>
-    <?php } else {
-      echo "<p>Nama Brand</p>";
-    } ?>
+    <a href="" class="navbar-brand py-2 d-flex align-items-center">
+      <?php if ($profile) { ?>
+        <img src="img/profil/<?php echo htmlspecialchars($profile['logo'])?>" alt="" class="me-2">
+        <h2 class="text-white m-0"><?php echo $profile['titlewebsite']; ?></h2>
+      <?php } else { ?>
+        <p class="m-0">Nama website</p>
+      <?php } ?>
+    </a>
 
-
-    <button class="navbar-toggler border-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+      <span class="fa fa-bars"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav mx-auto">
-        <li class="nav-item ">
-          <a class="nav-link " aria-current="page" href="#">Beranda</a>
-        </li>
-        <li class="nav-item dropdown ">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuAbout" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Tentang kami
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <div class="navbar-nav ms-auto py-0">
+        <a href="index.php" class="nav-item nav-link <?php echo ($current_page == 'index.php') ? 'active' : ''; ?>">Beranda</a>
+        <div class="nav-item dropdown">
+          <a href="#about" class="nav-link <?php echo ($current_page == 'about.php') ? 'active' : ''; ?>" data-bs-toggle="dropdown">
+            <span class="dropdown-toggle">Tentang kami</span>
           </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuAbout">
-            <li><a class="dropdown-item" href="tentangkami/profil.php">Profil </a></li>
-            <li><a class="dropdown-item" href="tentangkami/sejarah.php">Sejarah </a></li>
-            <li><a class="dropdown-item" href="tentangkami/struktur.php">Struktur Organisasi </a></li>
-            <li><a class="dropdown-item" href="#kegiatan">Kegiatan </a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown ">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuPerpustakaan" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Perpustakaan
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuPerpustakaan">
-            <li><a class="dropdown-item" href="perpustakaan/profil.php">Profil Perpustakaan</a></li>
-            <li><a class="dropdown-item" href="perpustakaan/layanan.php">Layanan Perpustakaan</a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown ">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuKearsipan" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Kearsipan
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuKearsipan">
-            <li><a class="dropdown-item" href="kearsipan/profil.html">Profil Kearsipan</a></li>
-            <li><a class="dropdown-item" href="kearsipan/layanan.html">Layanan Kearsipan</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#contact">Kontak</a>
-        </li>
-      </ul>
-      <div class="navbar-nav login-button ">
-        <button class="button_secondary"><a href="admin/index.php">Masuk</a></button>
+          <div class="dropdown-menu m-0">
+            <?php if (!empty($abouts)) {
+              foreach ($abouts as $about) { ?>
+                <a href="about.php?id=<?php echo urlencode($about['id_aboutus']); ?>" class="dropdown-item">
+                  <?php echo htmlspecialchars($about['title_aboutus']); ?>
+                </a>
+            <?php }
+            } else {
+              echo "<p>Tidak ada layanan</p>";
+            } ?>
+          </div>
+        </div>
+        <a href="service.php" class="nav-item nav-link <?php echo ($current_page == 'service.php') ? 'active' : ''; ?>">Layanan</a>
+        <a href="articel_blog.php" class="nav-item nav-link <?php echo ($current_page == 'articel_blog.php') ? 'active' : ''; ?>">Blog & Artikel</a>
+        <a href="contact.php" class="nav-item nav-link <?php echo ($current_page == 'contact.php') ? 'active' : ''; ?>">Kontak</a>
       </div>
+      <a href="admin/index.php" class="btn btn-light rounded-start rounded-bottom py-2 px-4 my-3 my-lg-0 flex-shrink-0">Masuk</a>
     </div>
   </div>
 </nav>
